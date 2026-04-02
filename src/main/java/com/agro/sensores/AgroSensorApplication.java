a8d172133ec35318c99f563f64cdb9803809c065
m530584 <julio.azevedo@bradesco.com.br>
2026-04-02 16:54:02 -0300

    C ofiguração do projeto

---
+++ src/main/java/com/agro/sensores/AgroSensorApplication.java
@@ -0,0 +1,13 @@
+package com.sensore.agro.grupo3back.agro_sensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroSensorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroSensorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sensore/agro/grupo3back/agro_sensor/AgroSensorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sensore.agro.grupo3back.agro_sensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroSensorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
